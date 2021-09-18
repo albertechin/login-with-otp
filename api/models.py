ee026c58de1f247b9b1b50b3063871424f1a31be
Albert Abraham <albert.abraham.official@gmail.com>
2021-09-18 19:30:45 +0530

    Add PhoneOTP model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 # Create your models here.
+class PhoneOTP(models.Model):
+    phone_regex = RegexValidator( regex   =r'^\+?1?\d{9,14}$', message ="Phone number must be entered in the format: '+999999999'. Up to 14 digits allowed.")
+    mobile = models.CharField(validators=[phone_regex], max_length=17, unique=True)
+    isVerified = models.BooleanField(blank=False, default=False)
+    counter = models.IntegerField(default=0, blank=False)   # For HOTP Verification
+
+    def __str__(self):
+        return str(self.mobile)
