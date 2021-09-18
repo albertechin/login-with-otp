9359a32ccd5e74a470e4c589606d6759716ed3be
Albert Abraham <albert.abraham.official@gmail.com>
2021-09-18 19:39:08 +0530

    Add API to verify OTP

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import GenerateOTP
+from .views import GenerateOTP, VerifyOTP
 
 urlpatterns = [
     
     path("get_otp/", GenerateOTP.as_view(), name="get_otp"),
+    path("verify_otp/", VerifyOTP.as_view(), name="view_otp"),
 
 ]
--- api/views.py
+++ api/views.py
@@ -32,3 +32,23 @@ class GenerateOTP(APIView):
         print(f'Generated OTP for {phone}: {OTP.now()}')
         # Using Multi-Threading send the OTP Using Messaging Services like Twilio or Fast2sms
         return Response({"OTP": OTP.now()}, status=200)  # Just for demonstration
+
+class VerifyOTP(APIView):
+
+    # This Method verifies the OTP
+    @staticmethod
+    def post(request):
+        phone = request.data["phone_number"]
+        try:
+            mobile = PhoneOTP.objects.get(mobile=phone)
+        except ObjectDoesNotExist:
+            return Response("User does not exist", status=404)  # False Call
+
+        keygen = generateKey()
+        key = base64.b32encode(keygen.returnValue(phone).encode())  # Generating Key
+        OTP = pyotp.TOTP(key,interval = EXPIRY_TIME)  # TOTP Model 
+        if OTP.verify(request.data["otp"]):  # Verifying the OTP
+            mobile.isVerified = True
+            mobile.save()
+            return Response("You are authorised", status=200)
+        return Response("OTP is wrong/expired", status=400)
